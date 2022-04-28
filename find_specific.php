4aedebe5cbe35e5d4d7ae4284a33435feee057e1
PrzeMazur95 <mazurkiewiczprzemyslaw95@gmail.com>
2022-04-28 10:52:04 +0200

    Add find specific pieces page

---
+++ find_specific.php
@@ -0,0 +1,51 @@
+<?php require_once("includes/header.php") ?>
+
+<?php
+
+  $regal = new pieceView();
+
+  $results = $regal->Show_specific();
+
+
+?>
+
+<div id="container-result">
+<h2>Kawałki produkcyjne</h2>
+  <table class="table">
+    <thead>
+      <tr>
+        <th scope="col">Id</th>
+        <th scope="col">Sku</th>
+        <th scope="col">Ilość</th>
+        <th scope="col">Regał</th>
+        <th scope="col">Opcje</th>
+      </tr>
+    </thead>
+    <tbody>
+
+    <?php while($row = $results->fetch(PDO::FETCH_ASSOC)) : ?>
+
+      <tr>
+        <td><?php echo $row['id']; ?></td>
+        <td><?php echo $row['sku']; ?></td>
+        <td><?php echo $row['ilosc']; ?></td>
+        <td><?php echo $row['nrregalu']; ?></td>
+
+        <td>
+        <a href="edit.php?id=<?php echo $row['id']; ?>" class="btn btn-primary btn-sm">Edytuj</a>
+        <a href="delete.php?id=<?php echo $row['id']; ?>" class="btn btn-danger btn-sm">Usuń</a>
+        </td>
+        
+      </tr>
+
+    <?php endwhile; ?>
+
+    </tbody>
+  </table>
+  <div class="opcje">
+                    
+                    <a href="index.php">Powrót do menu głównego</a>
+                                                           
+                    </div>
+</div>
+  
