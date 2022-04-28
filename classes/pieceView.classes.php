15ffd82d0c7569ba7de7d05bd07af313522b2959
PrzeMazur95 <mazurkiewiczprzemyslaw95@gmail.com>
2022-04-28 21:33:57 +0200

    Add find by id method

--- classes/pieceView.classes.php
+++ classes/pieceView.classes.php
@@ -18,5 +18,45 @@ class pieceView extends piece {
       return $results;
    }
 
+   public function Find_by_id($id){
+
+      if(isset($id)){
+
+          if(is_numeric($id)){
+
+            $piece = $this->Find_specific_piece($id);            
+
+              if($piece){
+
+                  $found_piece = new pieceController();
+                  $found_piece->id=$piece['id'];
+                  $found_piece->sku=$piece['sku'];
+                  $found_piece->ilosc=$piece['ilosc'];
+                  $found_piece->nrregalu=$piece['nrregalu'];
+
+                  return $found_piece;
+
+              } else {
+
+                  session::set("find_by_id", "Problem przy wyszukiwaniu w Db!");
+
+              }
+
+
+          } else {
+
+              session::set("find_by_id", "Podany numer nie jest liczbą!");
+
+          }
+
+      } else {
+
+          session::set("find_by_id", "Nie podano numeru kawałka!");
+
+      }
+
+  }
+
+
 }
 
