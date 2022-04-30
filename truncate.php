45031798469ba69669825b494b4c637ee28c6485
PrzeMazur95 <mazurkiewiczprzemyslaw95@gmail.com>
2022-04-30 21:10:58 +0200

    Add truncate page

--- truncate.php
+++ truncate.php
@@ -6,13 +6,25 @@
 
         $table = $_POST['tableName'];
 
+        if(pieceController::Truncate($table)){
+
+            session::set("truncate", "Tabela ".$_POST['tableName']." została pomyślnie wyczyszczona !");
+            header("location: index.php");
+
+        } else {
+
+            session::set("truncate", "Nie udało się wyczyścić tabeli!");
+            header("location: index.php");
+
+        } 
+
     } else {
 
         session::set("truncate", "Nie wybrano tabeli do wyczyszczenia!");
         header("location: index.php");
 
     }
-
+        
 ?>
 
 
