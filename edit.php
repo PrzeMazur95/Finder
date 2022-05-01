7faf99923d316cc2df8f7ef8daf49f90fcb6b7c6
PrzeMazur95 <mazurkiewiczprzemyslaw95@gmail.com>
2022-05-01 11:17:31 +0200

    Add labels, change placeholder to value, add session message

--- edit.php
+++ edit.php
@@ -7,6 +7,7 @@
     $piece = new pieceView();
 
     $choosen_piece = $piece->Find_by_id($_GET['id']);
+    
 
     }
 
@@ -32,20 +33,24 @@
 ?>
 
 <div id="container">
+    <?php echo session::get("update"); ?>
     <?php echo session::get("find_by_id"); ?>
 	<?php session::unset("find_by_id"); ?>
     <form action="" method="post">
         <p>Edytuj wybrany kawałek o id : <?php echo $choosen_piece->id;?></p>
-        <p>Poniej wyświetlone są jego aktualne dane</p>
+        <p>Poniżej wyświetlone są jego aktualne dane</p>
         <hr>
         <div class="form-group">
-            <input class="form-control" id="sku" name="sku" value="<?php $choosen_piece->sku;?>" placeholder="SKU : <?php echo $choosen_piece->sku;?>">
+            <label for="sku">Sku</label>
+            <input class="form-control" id="sku" name="sku" value="<?php echo $choosen_piece->sku;?>" required>
         </div>
         <div class="form-group">
-            <input class="form-control" id="ilosc" name="ilosc" value="<?php $choosen_piece->ilosc;?>" placeholder="Ilość : <?php echo $choosen_piece->ilosc;?>">
+            <label for="ilosc">Ilość</label>
+            <input class="form-control" id="ilosc" name="ilosc" value="<?php echo $choosen_piece->ilosc;?>" required>
         </div>
         <div class="form-group">
-            <input class="form-control" id="nrregalu" name="nrregalu" value="<?php $choosen_piece->nrregalu;?>" placeholder="Nr. Regału : <?php echo $choosen_piece->nrregalu;?>">
+            <label for="nrregalu">Nr. regału</label>
+            <input class="form-control" id="nrregalu" name="nrregalu" value="<?php echo $choosen_piece->nrregalu;?>" required>
         </div>
         <button type="submit" name="edit" class="btn btn-primary">Edytuj</button>
 
